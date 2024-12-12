acef6f045c0902cdc2436848004d4cce188c3cff
Mike Pullen <mike.pullen@gmail.com>
2024-12-12 15:44:24 -0600

    Update dog image API and enhance header styling

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const $gallery = document.querySelector(".gallery");
 
 const fetchDogData = fetch(
-  "https://api.thedogapi.com/v1/images/search?limit=10"
+  "https://dog.ceo/api/breed/labrador/images/random/50"
 );
 fetchDogData
   .then((response) => {
     return response.json();
   })
   .then((dogData) => {
-    const dogUrls = dogData.map((dog) => dog.url);
+    const dogUrls = dogData.message;
     $gallery.innerHTML = `${dogUrls
       .map((url, index) => `<img src="${url}" alt="dog photo ${index + 1}"/>`)
       .join("")}`;
